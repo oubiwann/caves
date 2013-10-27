25a0ca9115f9f6bd9d1880376cb2c0adb9c68e2a
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-27 15:23:12 -0500

    Updated comments.

--- src/caves/config.clj
+++ src/caves/config.clj
@@ -3,12 +3,12 @@
 ;; Determine the nature of the caves
 ;;  0-2:    give one huge cave with no features
 ;;  3:      gives the slightest hints of features
-;;  4:      a little features here and there, still mostly open
+;;  4:      a few interesting features here and there, still mostly open
 ;;  5:      lots of solid features, about 50/50 with open space
 ;;  6:      almost all closed spaces; most of the game will be digging
 ;;  7+:     unplayable! you don't want to use these numbers; there won't be
-;;          enough space to place all the entities
-(def smoothness 6)
+;;          enough space to place all the entities, and the game will hang :-/
+(def smoothness 4)
 
 ;; Key bindings for player movement
 (def key-west \a)
@@ -21,5 +21,7 @@
 (def key-south-east \c)
 
 ;; Key bindings for game controls
+(def key-insta-win :enter)
+(def key-insta-lose :backspace)
 (def key-quit \Q)
 (def key-reveal-zones \R)
